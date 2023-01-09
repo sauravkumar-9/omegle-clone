11432434fcf79cfb0de0ef0c86a94c1d8ba9a3ad
Saurav <connectsauravkumar@gmail.com>
2023-01-09 20:37:22 +0530

    User Name form cokkies

--- public/script.js
+++ public/script.js
@@ -19,9 +19,26 @@ showChat.addEventListener("click", () => {
   document.querySelector(".header__back").style.display = "block";
 });
 
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "Anonymous -";
+}
+
 const userFromSession = sessionStorage.getItem('userName');
 console.log("userFromSession ----", userFromSession)
-let user;
+let user = getCookie("userName");
 // if(userFromSession) {
 //   user = userFromSession;
 // } 
